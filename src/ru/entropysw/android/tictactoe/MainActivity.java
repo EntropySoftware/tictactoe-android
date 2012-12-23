f19341369f36f9086a0d94eeda79c611aaea811a
Michael <leor.thesweetvoice@gmail.com>
2012-12-24 00:40:01 +0400

    Table layout and dynamic field generation (not tested)

--- src/ru/entropysw/android/tictactoe/MainActivity.java
+++ src/ru/entropysw/android/tictactoe/MainActivity.java
@@ -2,6 +2,9 @@ package ru.entropysw.android.tictactoe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import ru.entropysw.android.R;
 
 /**
@@ -12,11 +15,27 @@ import ru.entropysw.android.R;
  * Основное activity. Пока работаем через него.
  */
 public class MainActivity extends Activity {
+    TableLayout game_table;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_l);
+        game_table = (TableLayout)findViewById(R.id.main_table);
+        makeGameField(3);
+    }
+
+    private void makeGameField(int num) {
+        for(int trCount=0; trCount < num; trCount++) {
+            TableRow tr = new TableRow(this);
+            for(int tdCount=0;tdCount < num; tdCount++) {
+                Button btn = new Button(this);
+                btn.setText(trCount%2 == 0 ? "X" : "O");
+                // bad code
+                tr.addView(btn, 100, 100);
+            }
+            game_table.addView(tr);
+        }
     }
 
 }
