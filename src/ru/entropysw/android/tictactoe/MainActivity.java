07101d405737f2b172af41d434b7a568a5bb16e6
Michael <leor.thesweetvoice@gmail.com>
2012-12-26 00:02:44 +0400

    Event Listener for buttons with Toasts

--- src/ru/entropysw/android/tictactoe/MainActivity.java
+++ src/ru/entropysw/android/tictactoe/MainActivity.java
@@ -1,10 +1,11 @@
 package ru.entropysw.android.tictactoe;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.view.Display;
+import android.view.View;
+import android.widget.*;
 import ru.entropysw.android.R;
 
 /**
@@ -22,20 +23,41 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_l);
         game_table = (TableLayout)findViewById(R.id.main_table);
-        makeGameField(3);
+        makeGameField(4);
     }
 
     private void makeGameField(int num) {
+        Display display = getWindowManager().getDefaultDisplay();
+        int width = display.getWidth();
+        int sqrSize = Math.round(width/num);
+
         for(int trCount=0; trCount < num; trCount++) {
             TableRow tr = new TableRow(this);
             for(int tdCount=0;tdCount < num; tdCount++) {
                 Button btn = new Button(this);
-                btn.setText(trCount%2 == 0 ? "X" : "O");
-                // bad code
-                tr.addView(btn, 100, 100);
+                btn.setText(tdCount%2 == 0 ? "X" : "O");
+                btn.setOnClickListener(getOnClickChange(btn));
+                tr.addView(btn, sqrSize, sqrSize);
             }
             game_table.addView(tr);
         }
+    }
+
+    private View.OnClickListener getOnClickChange(final Button button)  {
+        return new View.OnClickListener() {
+            public void onClick(View v) {
+                CharSequence currentSymbol = button.getText();
+                CharSequence newSymbol = currentSymbol == "X" ? "O" : "X";
+                button.setText(newSymbol);
+
+                Context context = getApplicationContext();
+                CharSequence text = "You click me! And now I`m "+newSymbol;
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        };
     }
 
 }
